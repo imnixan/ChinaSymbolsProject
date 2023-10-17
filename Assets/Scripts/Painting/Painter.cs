869d44a456ee722fde7c8248f3dab1d9f14e171c
imnixan <vcmvenom@gmail.com>
2023-10-17 17:41:08 +0700

    add drawing

--- Assets/Scripts/Painting/Painter.cs
+++ Assets/Scripts/Painting/Painter.cs
@@ -1,18 +1,119 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Painter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Material lineMat;
+
+    [SerializeField]
+    private float lineWidth;
+
+    [SerializeField]
+    private float tolerance;
+
+    private LineRenderer currentLineRender;
+    private List<Vector2> line = new List<Vector2>();
+    private List<int> deleteList = new List<int>();
+    private bool draw;
+    private Camera camera;
+    public int pointsUsed;
+
+    private void Start()
     {
-        
+        GetComponent<BoxCollider2D>().size = GetComponent<RectTransform>().rect.size;
+        camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (draw)
+        {
+            OnDraw();
+        }
+    }
+
+    private void OnDraw()
+    {
+        AddPointToLine(GetFingerPos());
+        line = SimplifyBezierCurve(line);
+        DrawLine();
+    }
+
+    private void OnMouseDown()
+    {
+        currentLineRender = Instantiate(new GameObject("Line"), transform)
+            .AddComponent<LineRenderer>();
+        InitLineRenderer(currentLineRender);
+        line.Add(GetFingerPos());
+        draw = true;
+    }
+
+    private Vector2 GetFingerPos()
+    {
+        return camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        draw = false;
+        pointsUsed += line.Count;
+        line.Clear();
+        Debug.Log("Painter up");
+    }
+
+    private void InitLineRenderer(LineRenderer lr)
+    {
+        lr.material = lineMat;
+        lr.startWidth = lineWidth;
+        lr.endWidth = lineWidth;
+        lr.startColor = Color.black;
+        lr.endColor = lr.startColor;
+        lr.useWorldSpace = true;
+    }
+
+    private void AddPointToLine(Vector2 point)
+    {
+        line.Add(point);
+    }
+
+    List<Vector2> SimplifyBezierCurve(List<Vector2> points)
+    {
+        List<Vector2> simplifiedPoints = new List<Vector2>();
+        simplifiedPoints.Add(points[0]);
+
+        for (int i = 1; i < points.Count - 2; i += 3)
+        {
+            Vector2 p0 = points[i];
+            Vector2 p1 = points[i + 1];
+            Vector2 p2 = points[i + 2];
+
+            float distance = Vector2.Distance(p0, p2);
+
+            if (distance > tolerance)
+            {
+                simplifiedPoints.Add(p0);
+                simplifiedPoints.Add(p1);
+                simplifiedPoints.Add(p2);
+            }
+        }
+
+        simplifiedPoints.Add(points[points.Count - 2]);
+        simplifiedPoints.Add(points[points.Count - 1]);
+
+        return simplifiedPoints;
+    }
+
+    private void DrawLine()
+    {
+        currentLineRender.positionCount = line.Count;
+        for (int i = 0; i < line.Count; i++)
+        {
+            currentLineRender.SetPosition(i, line[i]);
+        }
     }
 }
---
+++ Assets/Scripts/Tools.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class Tools : MonoBehaviour { }
